cd7f3a5d626e38260c59efb4de250ac343b8b791
Rob Lyon <rlyon@uidaho.edu>
2011-04-21 12:00:09 -0700

    added helper methods to get number of files and last updated

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,12 @@ class Folder < ActiveRecord::Base
 	belongs_to :user
 	has_many :assets, :dependent => :destroy
 	attr_accessible :name, :parent_id, :user_id
+	
+	def last_updated
+		updated_at
+	end
+	
+	def number_of_files
+		assets.length
+	end
 end
